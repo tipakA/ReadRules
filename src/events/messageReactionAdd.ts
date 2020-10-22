37a1771eb1c2f62946e4261e31f93a7a056d114d
tipakA <emka456@onet.pl>
2020-10-23 00:52:06 +0200

    Add failure reaction feature

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -6,22 +6,30 @@ import ReadRulesClient from '../index';
 async function messageReactionAddEvent(client: ReadRulesClient, reaction: MessageReaction, user: User) {
   if (reaction.message.guild?.id !== constants.ids.gating.guild) return;
 
-  if (reaction.emoji.name !== constants.ids.gating.emoji) return;
+  const emojis = constants.ids.gating.emoji.fail.concat(constants.ids.gating.emoji.pass);
+  if (!emojis.includes(reaction.emoji.name)) return;
   if (reaction.message.id !== constants.ids.gating.rulesMessage) return;
 
   const member = await reaction.message.guild!.members.fetch(user);
   if (!member) return console.error('Member who reacted could not be fetched!');
 
-  if (member.roles.cache.has(constants.ids.gating.joinRole)) {
-    const time = Date.now() - member.joinedTimestamp!;
-    const seconds = time / 1000;
-    const minutes = seconds / 60;
+  const time = Date.now() - member.joinedTimestamp!;
+  const seconds = time / 1000;
+  const minutes = seconds / 60;
 
-    let after: string;
-    if (minutes >= 60) after = `${(minutes / 60).toFixed(1)} hours`;
-    else if (seconds >= 60) after = `${(seconds / 60).toFixed(1)} minutes`;
-    else after = `${seconds.toFixed(1)} seconds`;
+  let after: string;
+  if (minutes >= 60) after = `${(minutes / 60).toFixed(1)} hours`;
+  else if (seconds >= 60) after = `${(seconds / 60).toFixed(1)} minutes`;
+  else after = `${seconds.toFixed(1)} seconds`;
 
+  if (constants.ids.gating.emoji.fail.includes(reaction.emoji.name)) {
+    try {
+      await user.send('Sorry, but you did not read the rules properly.');
+    } catch {} finally { // eslint-disable-line no-empty
+      await member.kick('failed the check');
+      await client.logChannel?.send(`${user} failed to react with proper emote after \`${after}\`.`);
+    }
+  } else if (constants.ids.gating.emoji.pass.includes(reaction.emoji.name)) {
     await member.roles.remove(constants.ids.gating.joinRole);
     await client.logChannel?.send(`${user} unlocked access to the server after \`${after}\`.`);
   }
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,11 +1,15 @@
+/* eslint-disable sort-keys */
 const constants = {
   ids: {
     gating: {
-      emoji: '✅',
+      emoji: {
+        fail: ['✅'],
+        pass: [ '💕', '💚' ],
+      },
       guild: '335286472921841665',
       joinRole: '758926599952924723',
-      rulesChannel: '753092087490019418',
-      rulesMessage: '753500011328962630',
+      rulesChannel: '768955020007571486',
+      rulesMessage: '768956704938786908',
     },
     owner: '245867108582293504',
     userlogChannel: '595538665774776370',
